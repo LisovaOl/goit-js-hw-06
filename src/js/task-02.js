5ea6a12192626f1c168a759a5b1ef14ad352672f
LisovaOl <lisova.olenka@gmail.com>
2022-07-23 14:55:53 +0300

    update

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -19,3 +19,11 @@ const ingredientEl = ingredients.map((element) => {
 
 listOfIngridients.append(...ingredientEl);
 console.log(listOfIngridients);
+
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+//     Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+//     Додасть назву інгредієнта як його текстовий вміст.
+//     Додасть елементу клас item.
+//     Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
--- src/js/task-07.js
+++ src/js/task-07.js
@@ -11,3 +11,9 @@ console.log(textEl);
 rangeEl.addEventListener("input", function () {
   textEl.style.fontSize = rangeEl.value + "px";
 });
+
+// Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text, оновлюючи властивість font-size. В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+
+// <input id="font-size-control" type="range" min="16" max="96" />
+// <br />
+// <span id="text">Abracadabra!</span>
