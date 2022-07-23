e88c897b4a3063f712322ec193686a251239ff57
LisovaOl <lisova.olenka@gmail.com>
2022-07-23 22:42:03 +0300

    t-07

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -3,14 +3,20 @@ console.log(rangeEl);
 const textEl = document.querySelector('#text');
 console.log(textEl);
 
+// ------  Варіант 1 -------
+
+rangeEl.addEventListener("input", function () {
+  textEl.style.fontSize = rangeEl.value + "px";
+});
+
+// ------  Варіант 2 -------
 
 // rangeEl.addEventListener("change", function () {
 //   textEl.style.fontSize = this.value + "px";
 // });
 
-rangeEl.addEventListener("input", function () {
-  textEl.style.fontSize = rangeEl.value + "px";
-});
+
+
 
 // Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text, оновлюючи властивість font-size. В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
 
