57a2e83867bd714cbe5778bc6c6d3faaf4f2f737
LisovaOl <lisova.olenka@gmail.com>
2022-07-26 17:11:47 +0300

    08

--- js/task-08.js
+++ js/task-08.js
@@ -4,15 +4,33 @@ formEl.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault(); //сторінка не повинна перезавантажуватися
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
-  const formData = new FormData(event.currentTarget);
+  if (email.value === "" || password.value === "") {
+    return alert("Будь ласка, заповніть всі поля!");
+  }
 
-  formData.forEach((email, password) => {
-    console.log("onFormSubmit -> name", email);
-    console.log("onFormSubmit -> name", password);
-  });
+  console.log(`{Email: ${email.value}, Password: ${password.value}}`);
   event.currentTarget.reset(); //очисти значення полів форми
 }
+
+// const formEl = document.querySelector(".login-form");
+
+// formEl.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault(); //сторінка не повинна перезавантажуватися
+
+//   const formData = new FormData(event.currentTarget);
+
+//   formData.forEach((email, password) => {
+//     console.log("onFormSubmit -> name", email);
+//     console.log("onFormSubmit -> name", password);
+//   });
+//   event.currentTarget.reset(); //очисти значення полів форми
+// }
 // Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
 // Під час відправлення форми сторінка не повинна перезавантажуватися.
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
